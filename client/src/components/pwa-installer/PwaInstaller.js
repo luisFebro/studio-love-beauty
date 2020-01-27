3bfd76b3776543cff3b0f583ab3e1f465da03a90
Luis Febro <mr.febro@gmail.com>
2020-01-26 23:12:34 -0400

    audit - performance - new pwa installet btn || new AOS - animation on scrolling library

--- client/src/components/pwa-installer/PwaInstaller.js
+++ client/src/components/pwa-installer/PwaInstaller.js
@@ -1,5 +1,5 @@
 import './style.css';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { isIos, isInStandaloneMode } from './utils';
 import { useStoreDispatch } from 'easy-peasy';
@@ -20,41 +20,52 @@ export default function PwaInstaller({ title, icon }) { // A2HS = App to HomeScr
     const [bannerVisible, setBannerVisible] = useState(true);
     const dispatch = useStoreDispatch();
 
-    let deferredPrompt = null;
-    const addBtn = document.querySelector('#btnAdd');
-    addBtn.style.display = 'none';
+    const handlePwa = () => {
+        let deferredPrompt = null;
+        const addBtn = document.querySelector('#panelAdd');
+        console.log("addBtn", addBtn);
+        addBtn.style.display = 'none';
 
 
-    window.addEventListener('beforeinstallprompt', (e) => {
-      // Prevent Chrome 67 and earlier from automatically showing the prompt
-      e.preventDefault();
-      // Stash the event so it can be triggered later.
-      deferredPrompt = e;
-      addBtn.style.display = 'block';
-    });
+        window.addEventListener('beforeinstallprompt', (e) => {
+          // Prevent Chrome 67 and earlier from automatically showing the prompt
+          e.preventDefault();
+          // Stash the event so it can be triggered later.
+          deferredPrompt = e;
+          addBtn.style.display = 'block';
 
-    window.addEventListener('appinstalled', (evt) => {
-      showSnackbar(dispatch, 'O app foi instalado com sucesso. Acesse o app na tela inicial do seu dispositivo', 'success', 6000)
-      setTimeout(() => closeWindow(), 7000)
-    });
+          addBtn.addEventListener('click', (e) => {
+            addBtn.style.display = 'none';
 
-    async function onPwaInstallerClick() {
-        if(deferredPrompt) {
-            // Show the prompt
-            deferredPrompt.prompt();
-            // Wait for the user to respond to the prompt
-            deferredPrompt.userChoice.then(function(choiceResult) {
-                if(choiceResult.outcome === 'accepted') {
-                    showSnackbar(dispatch, 'Instalando App...', 'success', 6000)
-                } else {
-                    showSnackbar(dispatch, 'A instalação foi cancelada.', 'warning')
-                }
+            if(deferredPrompt) {
+                // Show the prompt
+                deferredPrompt.prompt();
+                // Wait for the user to respond to the prompt
+                deferredPrompt.userChoice.then(function(choiceResult) {
+                    if(choiceResult.outcome === 'accepted') {
+                        showSnackbar(dispatch, 'Instalando App...', 'success', 6000)
+                    } else {
+                        showSnackbar(dispatch, 'A instalação foi cancelada.', 'warning')
+                    }
 
-                  deferredPrompt = null;
+                      deferredPrompt = null;
 
-            });
-        }
+                });
+            }
+
+          })
+        });
+
+        window.addEventListener('appinstalled', (evt) => {
+          showSnackbar(dispatch, 'O app foi instalado com sucesso. Acesse o app na tela inicial do seu dispositivo', 'success', 6000)
+          setTimeout(() => closeWindow(), 7000)
+        });
     }
+
+    useEffect(() => {
+        handlePwa();
+    }, [])
+
 
     const styles = {
         icon: {
@@ -95,22 +106,17 @@ export default function PwaInstaller({ title, icon }) { // A2HS = App to HomeScr
     const shouldRender = bannerVisible // && !isInStandaloneMode(); //&& isIos() && !isInStandaloneMode();
 
     return (
-      <div>
-        {shouldRender ? (
-          <div
-            id="btnAdd"
-            onClick={() => onPwaInstallerClick()}
-            className="add-to-home-banner"
-            data-aos="fade-up"
-            data-aos-duration="2000"
-           >
-            <div data-aos="flip-left" className="add-to-home-content">
-              {icon ? <img style={styles.icon} className="add-to-home-icon animated slideInLeft" src={icon} /> : null}
-              {showTitle()}
-            </div>
-            {showCloseBtn()}
-          </div>
-        ) : null}
+      <div
+        id="panelAdd"
+        className="add-to-home-banner"
+        data-aos="fade-up"
+        data-aos-duration="2000"
+       >
+        <div data-aos="flip-left" className="add-to-home-content">
+          {icon ? <img style={styles.icon} className="add-to-home-icon animated slideInLeft" src={icon} /> : null}
+          {showTitle()}
+        </div>
+        {showCloseBtn()}
       </div>
     );
 }

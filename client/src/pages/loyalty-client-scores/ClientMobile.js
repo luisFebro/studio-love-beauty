70d660749879dcf1ae00f4437f7705aff9952c4e
Luis Febro <mr.febro@gmail.com>
2020-01-31 23:49:17 -0400

    update - query string to detect if user came from register || split content for both people that came from register or by link - fixed scope in manifest

--- client/src/pages/loyalty-client-scores/ClientMobile.js
+++ client/src/pages/loyalty-client-scores/ClientMobile.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ClientMobile() {
     return (
         <div>
-            HELLO, I AM THE CLIENTE MOBILE
+            HELLO, I AM THE MOBILE VERSION...
         </div>
     );
 }

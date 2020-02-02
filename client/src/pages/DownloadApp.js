1fcbe58e74ec8af43f1735946719387343093cd9
Luis Febro <mr.febro@gmail.com>
2020-02-01 21:18:10 -0400

    mobile app - ignore push-to-refresh and added window.location.href to reload download page to make the banner to appear properly

--- client/src/pages/DownloadApp.js
+++ client/src/pages/DownloadApp.js
@@ -76,7 +76,7 @@ export default function DownloadApp({ match, location }) {
                 run={run}
                 setIsInstalled={setIsInstalled}
             />
-            <span className="text-right for-version-test">{"t3"}</span>
+            <span className="text-right for-version-test">{"t4"}</span>
         </div>
     );
 }

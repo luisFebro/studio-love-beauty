e66603006f5622a01a2d738bf2ff08cd4d87173b
Luis Febro <mr.febro@gmail.com>
2020-02-02 23:08:32 -0400

    inserted toast alerts for service work message finished

--- client/src/pages/mobile-app/ClientMobileApp.js
+++ client/src/pages/mobile-app/ClientMobileApp.js
@@ -3,7 +3,7 @@ import React from 'react';
 export default function ClientMobile() {
     return (
         <div className="text-white text-em-2-5 text-center text-default">
-            HELLO, ANGRY TEST
+            HELLO, ANGRY TEST TEST 3
         </div>
     );
 }

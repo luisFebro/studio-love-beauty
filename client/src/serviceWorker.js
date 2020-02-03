ea91122621635437e8e41a469a09866fa64adfb6
Luis Febro <mr.febro@gmail.com>
2020-02-02 20:33:24 -0400

    mobile app - new android - testing install alert || inserted toast alerts for service work message

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -14,19 +14,21 @@ import './components/vanilla-js/toastify/toastify.css';
 // opt-in, read https://bit.ly/CRA-PWA
 
 const showToastify = (text, duration, backgroundColor) => {
-    Toastify({
-      text: text || "Hello I am the toastify notifier...",
-      duration: duration || 4000,
-      className: "toastify",
-      fontWeight: 'bold',
-      avatar: `${CLIENT_URL}/favicon/android-chrome-256x256.png`,
-      close: true,
-      gravity: "bottom",
-      position: 'left',
-      backgroundColor: backgroundColor || "#34495e", // dark blue
-      stopOnFocus: true, // Prevents dismissing of toast on hover
-      onClick: function(){} // Callback after click
+    const runToast = Toastify({
+        text: text || "Hello I am the toastify notifier...",
+        duration: duration || 4000,
+        className: "toastify",
+        fontWeight: 'bold',
+        avatar: `${CLIENT_URL}/favicon/android-chrome-256x256.png`,
+        close: true,
+        gravity: "bottom",
+        position: 'left',
+        backgroundColor: backgroundColor || "#34495e", // dark blue
+        stopOnFocus: true, // Prevents dismissing of toast on hover
+        onClick: function(){} // Callback after click
     }).showToastify();
+
+    return runToast;
 }
 
 const isLocalhost = Boolean(
